eb913f25343aadf23fc232ac6f51e47c215bf7a2
Max Battcher <me@worldmaker.net>
2024-01-07 19:12:20 -0500

    Add a custom bulma build
    Shrink overall CSS by ~50kb (removing forms, grids, and most components as unnecessary for this demo) and removes the last CDN request.

--- _build.js
+++ _build.js
@@ -1,10 +1,18 @@
 import * as esbuild from 'esbuild'
+import { sassPlugin } from 'esbuild-sass-plugin'
 
 await esbuild.build({
   entryPoints: ['main.tsx'],
   bundle: true,
   outfile: 'main.js',
   format: 'esm',
+})
+
+await esbuild.build({
+  entryPoints: ['index.scss'],
+  bundle: true,
+  outfile: 'index.css',
+  plugins: [sassPlugin()],
 })
 
 // *** Test Modules ***
